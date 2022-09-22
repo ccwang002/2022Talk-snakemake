231714caf61d7ef8be89a06a61c029c39d9d6fff
ccwang002 <me@liang2.tw>
2022-09-22 07:37:54 +0000

    Update slides

--- js/highlightjs-snakemake.js
+++ js/highlightjs-snakemake.js
@@ -7,7 +7,7 @@ Category: common
 Modified based on https://github.com/snakemake/snakemake/blob/main/misc/vim/syntax/snakemake.vim
 and https://github.com/highlightjs/highlight.js/blob/main/src/languages/python.js
 */
-var snakemake = function(hljs) {
+function snakemake(hljs) {
     const regex = hljs.regex;
     const IDENT_RE = /[\p{XID_Start}_]\p{XID_Continue}*/u;
     const RESERVED_WORDS = [
@@ -487,5 +487,4 @@ var snakemake = function(hljs) {
         }
       ]
     };
-};
-
+}
